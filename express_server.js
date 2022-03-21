8546638542e0104f1874e78027219cd1119df199
Kai Meikle <kaimeikle123@gmail.com>
2022-03-21 23:59:23 +0000

    Create new shortURL

--- express_server.js
+++ express_server.js
@@ -38,8 +38,15 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let shortURL = generateRandomString();
+  console.log(req.body);
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 const generateRandomString = () => {
